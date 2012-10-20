21e6d42575d16835527511d80132ef5ed11c02cc
hotplugin <hotpluginenator@gmail.com>
2012-10-20 11:37:47 +0545

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProductTweeter::Application.config.secret_token = '1b36ab25c31936ffec5e1a0964f47371d543f4ff572676989fcf5f6782b823c089a9ff69cb17ace62ac763e7c078f6a3724ac3fc5053f6ad5cc3de5e206b71eb'
